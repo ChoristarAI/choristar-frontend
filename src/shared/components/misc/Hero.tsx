b98d42ea80b662d1acd6b58d73abb7b9e540d801
Isaiah Abiodun <isaiah.abiodun@outlook.com>
2025-09-22 22:04:49 +0100

    commit small changes

--- src/shared/components/misc/Hero.tsx
+++ src/shared/components/misc/Hero.tsx
@@ -1,14 +1,26 @@
+import { PhoneMockOne } from '@/assets/svg'
+
 interface HeroProps {}
 
 export const Hero: React.FC<React.PropsWithChildren<HeroProps>> = ({
   children,
 }) => {
   return (
-    <section className="z-10 h-[670px] w-full bg-[linear-gradient(180deg,#F9F8FB_70.67%,#FFFFFF_100%)]">
-      {/* <div className="absolute z-10 h-[670px] w-full bg-[linear-gradient(180deg,#F9F8FB_70.67%,#FFFFFF_100%)]"></div>
-      <div className="absolute -top-[119px] left-[146.74px] -z-10 h-[431.46px] w-[442.3px] rotate-[34.68deg] bg-[#FF647F0D] backdrop-blur-[150px]" />
-      <div className="absolute -top-[294px] -left-[244.23px] -z-10 h-[620.34px] w-[641.63px] rotate-[34.68deg] bg-[#6F46E50D] backdrop-blur-[150px]" />
-      <div>Loa</div> */}
+    <section className="section-container z-10 h-screen bg-[linear-gradient(180deg,#F9F8FB_70.67%,#FFFFFF_100%)]">
+      <div className="flex h-full w-full flex-col lg:flex-row">
+        <div className="flex h-full w-full items-center justify-center lg:w-1/3">
+          <h1 className="block">
+            <span className="block">Create.</span>
+            <span className="block">Lead.</span>
+            <span className="block">Harmonize.</span>
+          </h1>
+        </div>
+        <div className="w-full lg:w-2/3">
+          <div className="block h-72 w-64 align-middle">
+            <image width={250} height={400} href="/phone-left.png" />
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
--- src/shared/components/navigation/header.tsx
+++ src/shared/components/navigation/header.tsx
@@ -1,13 +1,20 @@
+import { Link } from '@tanstack/react-router'
+import { Button } from '../ui'
 import { ChoristarLogoIcon } from '@/assets/svg'
 
 export const Header = () => {
   return (
-    <header className="sticky top-0 z-10">
-      <div>
+    <header className="section-container sticky top-0 z-50 flex justify-between px-4 py-3 md:px-5 xl:px-8">
+      <Link className="flex items-center gap-4" to="/">
         <ChoristarLogoIcon />
-        <p>choristar</p>
-      </div>
-      <div></div>
+        <p className="font-semibold text-main-text capitalize">choristar</p>
+      </Link>
+      <Button
+        className="rounded-3xl px-4 py-3.5 font-medium md:px-6 lg:px-9"
+        variant="outline"
+      >
+        Join the waitlist
+      </Button>
     </header>
   )
 }
