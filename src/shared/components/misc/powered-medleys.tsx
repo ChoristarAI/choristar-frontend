6987c88e8629c481b1909d3944565af4e409ed40
Isaiah Abiodun <isaiah.abiodun@outlook.com>
2025-09-25 16:31:45 +0100

    push this up

---
+++ src/shared/components/misc/powered-medleys.tsx
@@ -0,0 +1,10 @@
+export const PoweredMedleys = () => (
+  <section className="section-container w-full px-4 md:px-8 lg:px-10 xl:px-16">
+    <div className="grid h-full w-full grid-cols-2">
+      <div className="col-span-2 w-full place-content-center place-items-center items-center-safe bg-amber-400 md:col-span-1">
+        <div className="min-h-80 bg-green-400">166515616</div>
+      </div>
+      <div className="col-span-2 bg-fuchsia-100 md:col-span-1"></div>
+    </div>
+  </section>
+)
